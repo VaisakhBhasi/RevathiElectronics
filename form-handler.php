661f1ab5209db57984f017002b4ce5c46672f817
Vaisakh Bhasi <132153214+VaisakhBhasi@users.noreply.github.com>
2025-09-10 16:14:31 +0530

    Update form-handler.php
    set email from

--- form-handler.php
+++ form-handler.php
@@ -4,14 +4,14 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];   
 
-$email_from = 'info@https://vaisakhbhasi.github.io/RevathiElectronics/contact.html';
+$email_from = 'info@revathielectronics.com';
 $email_subject = 'New Form Submission - Revathi Electronics';
 $email_body = "User Name: $name.\n".
                 "User Email: $visitor_email.\n".
                 "Subject: $subject.\n".
                 "User Message: $message.\n";
 
-$to = 'vaisakhbhasi16@gmail.com';
+$to = 'revathielectronics7@gmail.com';
 $headers = "From: $email_from \r\n";
 $headers .= "Reply-To: $visitor_email \r\n";
 mail($to, $email_subject, $email_body, $headers);
